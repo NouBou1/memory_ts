14036aa06468a9b58c8e9dc5c439d72c0951ab90
Noureddin Boussaada <n.boussaada92@gmail.com>
2026-08-13 16:33:16 +0200

    Refactor HTML structure, enhance card functionality, and update styles for improved gameplay experience

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
 import './styles/styles.scss';
 
-const board = document.querySelector<HTMLDivElement>('#board');
 
-if (!board) {
-  throw new Error('#board nicht gefunden – stimmt die ID in der index.html?');
-}
+init();
 
-board.textContent = 'Setup steht: Vite, TypeScript und SCSS sind verbunden.';
+function init() {
+ const fieldRef = document.getElementById('field');
+ if (fieldRef) {
+   fieldRef.addEventListener('click', e => {
+     const card = (e.target as HTMLElement).closest('.card') as HTMLButtonElement | null;
+     if (card && card.classList.contains('card')) {
+       card.classList.toggle('is-flipped');
+     }
+   });
+  }
+ }
